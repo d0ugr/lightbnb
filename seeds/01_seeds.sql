0246eba9c6d5d7ab6d9529b872205471125e1b91
e-s-c-m <e-s-c-m@Betsy.lan>
2020-04-01 16:24:50 -0400

    Add reservations seeds

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -16,5 +16,17 @@ INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover
 (7, 'Shine twenty', 'description', 'https://images.pexels.com/photos/2076739/pexels-photo-2076739.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2076739/pexels-photo-2076739.jpeg', 136.44, 1, 7, 8, Canada, '340 Dokto Park'    , 'Upfufa'   , 'Nova Scotia' , 29045, true),
 (8, 'Game fill'   , 'description', 'https://images.pexels.com/photos/1756826/pexels-photo-1756826.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/1756826/pexels-photo-1756826.jpeg', 234.28, 5, 6, 4, Canada, '834 Buwmi Road'    , 'Rotunif'  , 'Newfoundland And Labrador', 58224, true);
 
+INSERT INTO reservations (start_date, end_date, property_id, guest_id) VALUES
+'2018-09-11', '2018-09-26', 2, 3),
+'2019-01-04', '2019-02-01', 2, 2),
+'2021-10-01', '2021-10-14', 1, 4),
+'2014-10-21', '2014-10-21', 3, 5),
+'2016-07-17', '2016-08-01', 3, 4),
+'2018-05-01', '2018-05-27', 4, 8),
+'2022-10-04', '2022-10-23', 5, 1),
+'2015-09-13', '2015-09-30', 6, 8),
+'2023-05-27', '2023-05-28', 4, 2),
+'2023-04-23', '2023-05-02', 8, 1);
+
 
 
