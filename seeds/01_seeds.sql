3e7aca65d674b387c0b6f6eedad4b3111aa4ce19
e-s-c-m <e-s-c-m@Betsy.lan>
2020-04-01 16:07:43 -0400

    Update users seeds

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -1,9 +1,10 @@
 
 
 
-INSERT INTO users (email, password, name) VALUES ('test',   '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u', 'Kitties and Chickens');
-INSERT INTO users (email, password, name) VALUES ('t@e.st', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u', 'Testee');
-INSERT INTO users (email, password, name) VALUES ('4@2.0',  '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u', 'Four Twenty');
+INSERT INTO users (email, password, name) VALUES
+('test',   '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u', 'Kitties and Chickens'),
+('t@e.st', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u', 'Testee'),
+('4@2.0',  '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u', 'Four Twenty');
 
 
 
