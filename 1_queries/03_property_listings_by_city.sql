854d1818c327ded8808ca828d8e564aab7dc4263
e-s-c-m <e-s-c-m@Betsy.lan>
2020-04-01 17:30:26 -0400

    Add 03_property_listings_by_city.sql

---
+++ 1_queries/03_property_listings_by_city.sql
@@ -0,0 +1,14 @@
+
+
+
+SELECT   properties.title, AVG(property_reviews.rating) AS average_rating
+FROM     properties
+JOIN     property_reviews ON property_reviews.property_id = properties.id
+WHERE    properties.city LIKE '_ancouver'
+GROUP BY properties.id
+HAVING   AVG(property_reviews.rating) >= 4
+ORDER BY properties.cost_per_night
+LIMIT    10;
+
+
+
